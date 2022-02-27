cadaff6fef02bd4bfb12173dcc1897eeaebead6b
root <root@ip-172-31-85-253.ec2.internal>
2022-02-27 02:40:23 +0000

    fixes cors

--- src/main/java/com/oz/demojar/api/PersonController.java
+++ src/main/java/com/oz/demojar/api/PersonController.java
@@ -19,25 +19,68 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.persistence.RollbackException;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
+@CrossOrigin(maxAge = 3600)
 @RequestMapping("api/vi/person")
 @RestController
-
 public class PersonController {
 
-    private final String cors="http://www.ozdev.net";
+    private final String cors="http://www.ozdev.net, http://localhost:4200";
     private final PersonService personService;
     private final CountryService countryService;
+    private HttpServletRequest request;
     //private final Validator validator;
+    
+    public String getIpAddress(HttpServletRequest request) {
+        String ip = request.getHeader("X-Forwarded-For");
+        if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {
+            ip = request.getHeader("HTTP_X_FORWARDED");
+        }
+        if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {
+            ip = request.getHeader("HTTP_X_CLUSTER_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {
+            ip = request.getHeader("HTTP_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {
+            ip = request.getHeader("HTTP_FORWARDED");
+        }
+        if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {
+            ip = request.getHeader("HTTP_VIA");
+        }
+        if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {
+            ip = request.getHeader("REMOTE_ADDR");
+        }
+        if (ip == null || ip.length() == 0 || ip.equalsIgnoreCase("unknown")) {
+            ip = request.getRemoteAddr();
+        }
+        if (ip.equals("0:0:0:0:0:0:0:1")) ip = "127.0.0.1";
+        if (ip.chars().filter($ -> $ == '.').count() != 3) ip = "Illegal IP";
+        return ip;
+    }
 
     @Autowired
-    public PersonController(PersonService personService, CountryService countryService) {
+    public PersonController(PersonService personService, CountryService countryService, HttpServletRequest httpRequest) {
         this.personService = personService;
         this.countryService = countryService;
+        this.request = httpRequest;
 
         //ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
         //this.validator = factory.getValidator();
@@ -55,12 +98,14 @@ public class PersonController {
     }
 
     @GetMapping
-    @CrossOrigin(origins = cors)
     public List<Person> getAllPeople() {
+	String ip = this.getIpAddress(this.request);    
+        System.out.println("request from address: " + ip);
         List<Person> persons = personService.getAllPeople();
         return persons;
     }
 
+    @CrossOrigin(origins = cors)
     @GetMapping(path = "/anns")
     public List<Person> getAllAnns() {
         Collection<Person> persons = personService.findAllAnns();
@@ -68,7 +113,6 @@ public class PersonController {
     }
 
     @GetMapping(path = "{id}")
-    @CrossOrigin(origins = cors)
     public Person selectPersonById(@PathVariable("id") Long id) {
         if(id.equals("undefined")) { System.out.println("undefined");return null; }
         System.out.println(id);
@@ -76,13 +120,11 @@ public class PersonController {
     }
 
     @DeleteMapping(path = "{id}")
-    @CrossOrigin(origins = cors)
     public int deletePersonById(@PathVariable("id") Long id) {
         return personService.deletePersonById(id);
     }
 
     @PutMapping(path = "{id}")
-    @CrossOrigin(origins = cors)
     public Person updatePersonById(@PathVariable("id") Long id, @RequestBody Person person) {
         return personService.updatePersonById(id, person);
     }
@@ -101,7 +143,6 @@ public class PersonController {
     }
 
     @PutMapping  (path="bycountry/{cid}", consumes = MediaType.APPLICATION_JSON_VALUE)
-    @CrossOrigin(origins = cors)
     public List<Person> getPeopleByCountry(@PathVariable("cid") Long cid) {
         System.out.println("cid :"+cid);
         Collection<Person> persons = personService.findPersonsWithPassportsByCountry(cid);
@@ -124,7 +165,6 @@ public class PersonController {
     }
 
     @GetMapping("/findlast")
-    @CrossOrigin(origins = cors)
     public long findLastId() {
         Long id = personService.findLastId();
         System.out.println();
