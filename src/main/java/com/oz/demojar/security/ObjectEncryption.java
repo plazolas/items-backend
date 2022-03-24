07665c8dfdc59f09353664626977ef7728bf3c93
Oswaldo J Plazola <oplazola@hotmail.com>
2022-03-24 16:38:57 -0400

    turns  on debug for specific dependency

---
+++ src/main/java/com/oz/demojar/security/ObjectEncryption.java
@@ -0,0 +1,47 @@
+package com.oz.demojar.security;
+
+
+
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.IOException;
+import java.io.Serializable;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Random;
+
+public class ObjectEncryption {
+
+    public static SealedObject encryptObject(String algorithm, Serializable object,
+                                             SecretKey key, IvParameterSpec iv) throws NoSuchPaddingException,
+            NoSuchAlgorithmException, InvalidAlgorithmParameterException,
+            InvalidKeyException, IOException, IllegalBlockSizeException {
+
+        Cipher cipher = Cipher.getInstance(algorithm);
+        cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+        SealedObject sealedObject = new SealedObject(object, cipher);
+        return sealedObject;
+    }
+
+    public static Serializable decryptObject(String algorithm, SealedObject sealedObject,
+                                             SecretKey key, IvParameterSpec iv) throws NoSuchPaddingException,
+            NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException,
+            ClassNotFoundException, BadPaddingException, IllegalBlockSizeException,
+            IOException {
+
+        Cipher cipher = Cipher.getInstance(algorithm);
+        cipher.init(Cipher.DECRYPT_MODE, key, iv);
+        Serializable unsealObject = (Serializable) sealedObject.getObject(cipher);
+        return unsealObject;
+    }
+
+    public static Key getRandomKey(String cipher, int keySize) {
+        byte[] randomKeyBytes = new byte[keySize / 8];
+        Random random = new Random();
+        random.nextBytes(randomKeyBytes);
+        return new SecretKeySpec(randomKeyBytes, cipher);
+    }
+}
