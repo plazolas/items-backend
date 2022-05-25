a625e6baa79c61834b5752b152ee238d7c228bca
Oswaldo J Plazola <oplazola@hotmail.com>
2022-05-25 15:38:28 -0400

    removes unused imports

--- src/main/java/com/oz/demojar/DemojarApplication.java
+++ src/main/java/com/oz/demojar/DemojarApplication.java
@@ -1,10 +1,5 @@
 package com.oz.demojar;
 
-import com.oz.demojar.mysqlDatasource.CountryRepository;
-import com.oz.demojar.mysqlDatasource.PassportRepository;
-import com.oz.demojar.mysqlDatasource.PersonRepository;
-import com.oz.demojar.mysqlDatasource.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
