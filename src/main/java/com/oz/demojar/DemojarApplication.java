ef0ed5b75789ffdd3a5c23a795593ee0f33359ed
Oswaldo J Plazola <oplazola@hotmail.com>
2022-06-15 17:20:57 -0400

    adds exitCodeGenerator, listBeansPresence

--- src/main/java/com/oz/demojar/DemojarApplication.java
+++ src/main/java/com/oz/demojar/DemojarApplication.java
@@ -3,6 +3,7 @@ package com.oz.demojar;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.ApplicationRunner;
 
+import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -13,6 +14,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 // Use the @ComponentScan annotation along with the @Configuration annotation to specify the packages that we want to be scanned.
@@ -32,10 +37,20 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class DemojarApplication {
 	private static ApplicationContext applicationContext;
 
+	@Bean
+	public ExitCodeGenerator exitCodeGenerator() {
+		return () -> 0;
+	}
+
 	// System.setProperty("server.servlet.context-path", "/myapp");
 	public static void main(String[] args) {
 		applicationContext = SpringApplication.run(DemojarApplication.class, args);
-		listBeansPresence("springframework");
+		List<String> partial = Arrays.asList(
+				"Controller",
+				"Service",
+				"erv"
+		);
+		listBeansPresence(partial);
 	}
 
 	@Bean
@@ -51,13 +66,17 @@ public class DemojarApplication {
 
 			log.info("Environment: " + environment.getProperty("spring.config.activate.on-profile"));
 			log.info("Datasource:  " + environment.getProperty("spring.datasource.url"));
+			log.info("DevTools:  "   + environment.getProperty("spring.devtools.restart.enabled"));
 		};
 	}
 
-	private static void listBeansPresence(String partial) {
+	private static void listBeansPresence(List<String> partials) {
 		String[] beans = applicationContext.getBeanDefinitionNames();
-		for (String beanName : beans) {
-			if(!beanName.contains(partial)) System.out.println(beanName);
+		for (String dependency : partials) {
+			for (String beanName : beans) {
+				if (beanName.contains(dependency))
+					System.out.println(beanName);
+			}
 		}
 	}
 
